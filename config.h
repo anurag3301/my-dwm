5df25c7d018e8c4e4126fb60122384c9326c9850
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-13 22:03:29 +0530

    Added strict rules

--- config.h
+++ config.h
@@ -27,8 +27,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "discord",  NULL,       NULL,       1 << 2,       0,            1 },
+	{ "Spotify",  NULL,       NULL,       1 << 3,       0,            1 },
+	{ "TelegramDesktop",NULL, NULL,       1 << 5,       0,            1 },
 };
 
 /* layout(s) */
