ffe37b0bff032f6f74a0038c222f25ddbb583d49
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-07-13 18:54:04 +0530

    Remove conflicting keybinding

--- config.def.h
+++ config.def.h
@@ -103,7 +103,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
