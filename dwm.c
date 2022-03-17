c1cd554d538fe302c4e617e326d160cb0b2028ac
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-03-17 10:18:21 +0530

    Added urg-border patch

--- dwm.c
+++ dwm.c
@@ -60,7 +60,8 @@
 /* enums */
 enum { CurNormal, CurResize, CurMove, CurLast }; /* cursor */
 enum { SchemeNorm, SchemeSel, SchemeRed, SchemeYellow, SchemeOrange, 
-    SchemeGreen, SchemePurple, SchemePink, Schemeblue, SchemeDarkRed}; /* color schemes */
+       SchemeGreen, SchemePurple, SchemePink, Schemeblue, SchemeDarkRed,
+       SchemeUrg}; /* color schemes */
 enum { NetSupported, NetWMName, NetWMState, NetWMCheck,
        NetWMFullscreen, NetActiveWindow, NetWMWindowType,
        NetWMWindowTypeDialog, NetClientList, NetLast }; /* EWMH atoms */
@@ -2064,8 +2065,11 @@ updatewmhints(Client *c)
 		if (c == selmon->sel && wmh->flags & XUrgencyHint) {
 			wmh->flags &= ~XUrgencyHint;
 			XSetWMHints(dpy, c->win, wmh);
-		} else
+		} else {
 			c->isurgent = (wmh->flags & XUrgencyHint) ? 1 : 0;
+			if (c->isurgent)
+				XSetWindowBorder(dpy, c->win, scheme[SchemeUrg][ColBorder].pixel);
+        }
 		if (wmh->flags & InputHint)
 			c->neverfocus = !wmh->input;
 		else
