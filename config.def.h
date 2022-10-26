ecbb4a614b2e660cd0c8395b3178069d6eb63de8
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2022-10-26 10:58:09 +0530

    Added one more tag for steam and rearranged the rules for applications

--- config.def.h
+++ config.def.h
@@ -46,7 +46,7 @@ static const char *colors[][3]      = {
 static const int statmonval = 0;
 
 /* tagging */
-static const char *tags[] = { " ", " ", " ", " ", " ", "ﬧ ", "ﭮ ", " "};
+static const char *tags[] = { " ", " ", " ", " ", " ", " ", "ﬧ ", "ﭮ ", " "};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -54,12 +54,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class         instance    title       tags mask     isfloating   monitor */
-  { "discord",         NULL,       NULL,       1 << 6,       0,            0},
-  { "Spotify",         NULL,       NULL,       1 << 7,       0,            1},
-  { "kdeconnect.app",  NULL,       NULL,       1 << 6,       0,            1},
-  { "Element",         NULL,       NULL,       1 << 5,       0,            1},
-  { "Xournalpp",       NULL,       NULL,       1 << 7,       0,            0},
-  { "Thunderbird",     NULL,       NULL,       1 << 5,       0,            0}
+  { "discord",         NULL,       NULL,       1 << 7,       0,            0},
+  { "Spotify",         NULL,       NULL,       1 << 8,       0,            1},
+  { "kdeconnect.app",  NULL,       NULL,       1 << 7,       0,            1},
+  { "Xournalpp",       NULL,       NULL,       1 << 8,       0,            0},
+  { "thunderbird",     NULL,       NULL,       1 << 6,       0,            1}
 };
 
 /* layout(s) */
