4ab0b1cb03a2c3d3981d1b2017cb3be8f8edad8c
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2022-12-23 09:28:47 +0530

    Trying number tag identifier instead of nerd fonts

--- config.def.h
+++ config.def.h
@@ -46,7 +46,8 @@ static const char *colors[][3]      = {
 static const int statmonval = 0;
 
 /* tagging */
-static const char *tags[] = { " ", " ", " ", " ", " ", "ﬧ ", " ", "ﭮ ", " "};
+// static const char *tags[] = { " ", " ", " ", " ", " ", "ﬧ ", " ", "ﭮ ", " "};
+static const char *tags[] = { "|1|", "|2|", "|3|", "|4|", "|5|", "|6|", "|7|", "|8|", "|9|"};
 
 static const Rule rules[] = {
 	/* xprop(1):
