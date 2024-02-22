ef7d1a97e4a8eee47816c2cfef44e7238b6c970d
Anurag Kumar Singh <anurag3301.0x0@gmail.com>
2024-02-22 12:43:37 +0530

    testing center first window patch

--- dwm.c
+++ dwm.c
@@ -96,7 +96,7 @@ struct Client {
 	int basew, baseh, incw, inch, maxw, maxh, minw, minh;
 	int bw, oldbw;
 	unsigned int tags;
-	int isfixed, iscentered, isfloating, isurgent, neverfocus, oldstate, isfullscreen;
+	int isfixed, iscentered, isfloating, isurgent, neverfocus, oldstate, isfullscreen, CenterThisWindow;
 	Client *next;
 	Client *snext;
 	Monitor *mon;
@@ -151,6 +151,7 @@ typedef struct {
 	const char *title;
 	unsigned int tags;
 	int iscentered;
+    int CenterThisWindow;
 	int isfloating;
 	int monitor;
 } Rule;
@@ -310,6 +311,7 @@ applyrules(Client *c)
 	c->iscentered = 0;
 	c->isfloating = 0;
 	c->tags = 0;
+    c->CenterThisWindow = 0;
 	XGetClassHint(dpy, c->win, &ch);
 	class    = ch.res_class ? ch.res_class : broken;
 	instance = ch.res_name  ? ch.res_name  : broken;
@@ -322,6 +324,7 @@ applyrules(Client *c)
 		{
 			c->iscentered = r->iscentered;
 			c->isfloating = r->isfloating;
+            c->CenterThisWindow = r->CenterThisWindow;
 			c->tags |= r->tags;
 			for (m = mons; m && m->num != r->monitor; m = m->next);
 			if (m)
@@ -1756,6 +1759,12 @@ tile(Monitor *m)
 			if (ty + HEIGHT(c) + m->gap->gappx < m->wh)
 				ty += HEIGHT(c) + m->gap->gappx;
 		}
+    if (n == 1 && selmon->sel->CenterThisWindow)
+         resizeclient(selmon->sel,
+                 (selmon->mw - selmon->mw * 0.5) / 2,
+                 (selmon->mh - selmon->mh * 0.5) / 2,
+                 selmon->mw * 0.5,
+                 selmon->mh * 0.5);
 }
 
 void
